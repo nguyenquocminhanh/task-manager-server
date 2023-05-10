2aff6680fbf403fb0b92159f78c3a17f84b94943
Minh Nguyen <minhanh.nguyenquoc@gmail.com>
2023-05-10 02:16:57 -0400

    add cors

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ const task = require('./routes/task');
 const app = express();
 
 app.use(cors());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://task-manager-minh-nguyen.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+  
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
