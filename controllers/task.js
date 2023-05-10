207364c894fe2fdbc9b1812619d3675280fa2a07
Minh Nguyen <minhanh.nguyenquoc@gmail.com>
2023-05-10 13:18:04 -0400

    update delete and put by using sequelize instead of pool

--- controllers/task.js
+++ controllers/task.js
@@ -1,14 +1,4 @@
 const Task = require('../models/Task');
-const { Pool } = require('pg');
-
-const pool = new Pool({
-  user: process.env.USERNAME,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT, // your db port
-});
-
 
 exports.createTask = async (req, res) => {
     try {
@@ -30,7 +20,7 @@ exports.createTask = async (req, res) => {
          })
     } catch (err) {  
         console.log(err);
-        res.status(500).json({error: 'Something went wrong'});
+        res.status(500).json({error: err});
     }
 }
 
@@ -43,7 +33,7 @@ exports.getAllTasks = async (req, res) => {
         return res.status(200).json({tasks: allTasks});
     } catch (error) {
         console.log(err);
-        return res.status(500).json({error: 'Something  wrong'});
+        return res.status(500).json({error: error});
     }
 }
 
@@ -57,29 +47,24 @@ exports.getOneTask = async (req, res) => {
         } return res.status(200).json({task: task});
     } catch (error) {
         console.log(error);
-        return res.status(500).json({error: 'Something went wrong'});
+        return res.status(500).json({error: error});
     }
 }
 
 exports.deleteTask = async (req, res) => {
-    const taskId = req.params.taskId;
+    const taskId = req.params.taskId; 
+    // model Task from sequelize from Postgresql
+    const task = await Task.findOne({ where: { id: taskId } });
 
-    const query = {
-        text: 'DELETE FROM public."Tasks" WHERE id = $1',
-        values: [taskId],
-    };
-    let client;
-    
+    if (!task) {
+        return res.status(500).json({error: 'Task not found!'});
+    }
+
     try {
-        client = await pool.connect();
-        const result = await pool.query(query);
-        res.status(204).json({message: `Deleted ${result} row(s) successfully`}); // No Content
+        await task.destroy();
+        res.status(204).json({message: `Deleted successfully`});
     } catch (error) {
         return res.status(500).json({error: error});
-    } finally {
-        if (client) {
-            client.release();
-        }
     }
 }
 
@@ -87,28 +72,32 @@ exports.updateTask = async (req, res) => {
     const taskId = req.params.taskId;
     const { title, description, dueDate  } = req.body;
    
+    // model Task from sequelize from Postgresql
     const task = await Task.findOne({ where: { id: taskId } });
 
-    let query = {};
-
-    // update title, description, dueDate
-    if (title) {
-        query = {
-            text: `UPDATE public."Tasks" SET title = $1, description = $2, "dueDate" = $3 WHERE id = $4`,
-            values: [title, description, dueDate, taskId],
-        };
-    } else {
-        query = {
-            text: `UPDATE public."Tasks" SET completed = ${!task.completed} WHERE id = $1`,
-            values: [taskId],
-        };
+    if (!task) {
+        return res.status(500).json({error: 'Task not found!'});
     }
 
-    try {
-        const result = await pool.query(query);
-        res.status(200).json({message: `Updated task with id ${taskId} successfuilly`});
-    } catch (err) {
-        console.log(err);
-        return res.status(500).json({error: 'Something went wrong'});
+    if (!title) {       // update completed
+        try {
+            task.completed = !task.completed;
+            await task.save();
+            res.status(200).json({message: `Updated task with id ${taskId} successfuilly`})
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json({error: err});
+        }
+    } else {        // update title, description, dueDate
+        try {
+            task.title = title;
+            task.description = description;
+            task.dueDate = dueDate;
+            await task.save();
+            res.status(200).json({message: `Updated task with id ${taskId} successfuilly`});
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json({error: err});
+        }
     }
 }
