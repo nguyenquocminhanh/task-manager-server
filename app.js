e6d2411a1bee212a3dda332bf77a30d1a817f54d
Minh Nguyen <minhanh.nguyenquoc@gmail.com>
2023-05-20 20:53:33 -0400

    Add Task Team Manager

--- app.js
+++ app.js
@@ -30,6 +30,11 @@ app.use(bodyParser.json());
 
 //Middleware to handle API requests
 app.use(cors());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 app.use('/api/auth', auth);
 app.use('/api/tasks', task);
 app.use('/api/teams', team);
